1dc256a4ada64c0276c4238e3ca36b1049068465
Данил <danil.beloborodoff@yandex.ru>
2021-07-24 07:27:05 +0300

    все сделал начинаю тестирование

--- build/js/script.js
+++ build/js/script.js
@@ -1,36 +1,63 @@
-let orderCallButton = document.querySelector(".contacts__order-call");
-let modal = document.querySelector(".modal");
-let form = modal.querySelector("form");
-let modalCloseButton = document.getElementById("close-button");
+'use strict';
+var orderCallButton = document.querySelector('.contacts__order-call');
+var modal = document.querySelector('.modal');
+var form = modal.querySelector('form');
+var modalCloseButton = document.getElementById('close-button');
+var footerNav = document.querySelector('.main-info__nav');
+var footerNavHeader = footerNav.querySelector('h3');
+var footerContacts = document.querySelector('.main-info__contacts');
+var footerContactsHeader = footerContacts.querySelector('h3');
+var modalName = document.getElementById('modalName');
 
-//Открытие и закрытие модального окна
-orderCallButton.addEventListener(("click"), function (e) {
+var maskPhone = {
+  0: '+',
+  2: '(',
+  6: ')',
+  10: '-',
+  13: '-'
+};
+
+var phoneRegExp = /[A-Za-zА-Яа-яЁё.!<>%$?:"'*,~|_№;=]/;
+
+if (footerNav && footerContacts) {
+  footerNav.classList.remove('main-info__nav--no-js');
+  footerContacts.classList.remove('main-info__contacts--no-js');
+  footerNav.classList.add('main-info__nav--closed');
+  footerContacts.classList.add('main-info__contacts--closed');
+}
+
+// Открытие и закрытие модального окна
+orderCallButton.addEventListener(('click'), function (e) {
   e.preventDefault();
-  modal.classList.remove("modal--closed");
-  modal.classList.add("modal--opened");
-})
+  modal.classList.remove('modal--closed');
+  modal.classList.add('modal--opened');
+  modalName.focus();
+});
 
-modal.addEventListener(("click"), function (evt) {
+function closeModal() {
+  modal.classList.add('modal--closed');
+  modal.classList.remove('modal--opened');
+}
+
+modal.addEventListener(('click'), function (evt) {
   if (evt.target === modal && evt.target !== form) {
-    modal.classList.add("modal--closed");
-    modal.classList.remove("modal--opened");
+    closeModal();
   }
 });
 
-modalCloseButton.addEventListener(("click"), function (evt) {
-  modal.classList.add("modal--closed");
-  modal.classList.remove("modal--opened");
+modalCloseButton.addEventListener(('click'), function () {
+  closeModal();
 });
 
-document.addEventListener(("keydown"), function (evt) {
-  if (evt.key === "Escape") {
-    modal.classList.add("modal--closed");
-    modal.classList.remove("modal--opened");
+document.addEventListener(('keydown'), function (evt) {
+  if (evt.key === 'Escape') {
+    modal.classList.add('modal--closed');
+    modal.classList.remove('modal--opened');
   }
 });
 
-//Плавная прокрутка к якорю
-let anchors = document.querySelectorAll('a');
+// Плавная прокрутка к якорю
+var anchors = document.querySelectorAll('a');
 if (anchors) {
   for (var i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener('click', function (e) {
@@ -44,23 +71,23 @@ if (anchors) {
   }
 }
 
-//Состояние чекбокса форма на странице
-let formPersonalDataCheckbox = document.querySelector(".form__checkbox");
-formPersonalDataCheckbox.addEventListener(("click"), function (evt) {
-  evt.target.parentNode.classList.toggle("form__checkbox--checked");
-})
+// Состояние чекбокса форма на странице
+var formPersonalDataCheckbox = document.querySelector('.form__checkbox');
+formPersonalDataCheckbox.addEventListener(('click'), function (evt) {
+  evt.target.parentNode.classList.toggle('form__checkbox--checked');
+});
 
-//Состояние чекбокса модальное окно
-let modalPersonalDataCheckbox = document.querySelector(".modal__checkbox");
-modalPersonalDataCheckbox.addEventListener(("click"), function (evt) {
-  evt.target.parentNode.classList.toggle("modal__checkbox--checked");
-})
+// Состояние чекбокса модальное окно
+var modalPersonalDataCheckbox = document.querySelector('.modal__checkbox');
+modalPersonalDataCheckbox.addEventListener(('click'), function (evt) {
+  evt.target.parentNode.classList.toggle('modal__checkbox--checked');
+});
 
-//Форма на странице
-let nameInputForm = document.getElementById("formName");
-let phoneInputForm = document.getElementById("formPhone");
-let questionInputForm = document.getElementById("formQuestion");
-let sendFormButtom = document.getElementById("sendForm");
+// Форма на странице
+var nameInputForm = document.getElementById('formName');
+var phoneInputForm = document.getElementById('formPhone');
+var questionInputForm = document.getElementById('formQuestion');
+var sendFormButtom = document.getElementById('sendForm');
 
 if (nameInputForm && phoneInputForm && questionInputForm && sendFormButtom) {
   nameInputForm.addEventListener('input', function () {
@@ -71,28 +98,21 @@ if (nameInputForm && phoneInputForm && questionInputForm && sendFormButtom) {
     }
   });
 
-  phoneInputForm.addEventListener('input', function () {
-    if (phoneInputForm.validity.patternMismatch) {
-      phoneInputForm.setCustomValidity('Введите телефон в формате +7 ХХХ ХХХ ХХ ХХ');
-    } else {
-      phoneInputForm.setCustomValidity('');
-    }
-  });
-
   sendFormButtom.addEventListener('click', function (e) {
     e.preventDefault();
     localStorage.setItem(nameInputForm.name, nameInputForm.value);
     localStorage.setItem(phoneInputForm.name, phoneInputForm.value);
     localStorage.setItem(questionInputForm.name, questionInputForm.value);
-    console.log(localStorage)
+    closeModal();
+    form.classList.add('modal__form--sent');
   });
 }
 
-//Модальное окно
-let nameInputModal = document.getElementById("modalName");
-let phoneInputModal = document.getElementById("modalPhone");
-let questionInputModal = document.getElementById("modalQuestion");
-let sendModalButtom = document.getElementById("sendModal");
+// Модальное окно
+var nameInputModal = document.getElementById('modalName');
+var phoneInputModal = document.getElementById('modalPhone');
+var questionInputModal = document.getElementById('modalQuestion');
+var sendModalButtom = document.getElementById('sendModal');
 
 if (nameInputModal && phoneInputModal && questionInputModal && sendModalButtom) {
   nameInputModal.addEventListener('input', function () {
@@ -111,37 +131,60 @@ if (nameInputModal && phoneInputModal && questionInputModal && sendModalButtom) 
     }
   });
 
-  sendModalButtom.addEventListener('click', function (e) {
-    e.preventDefault();
+  sendModalButtom.addEventListener('click', function () {
     localStorage.setItem(nameInputModal.name, nameInputModal.value);
     localStorage.setItem(phoneInputModal.name, phoneInputModal.value);
     localStorage.setItem(questionInputModal.name, questionInputModal.value);
-    console.log(localStorage)
   });
 }
 
 
-//Аккардеон
+// Аккардеон
 
-let footerNav = document.querySelector(".main-info__nav");
-let footerContacts = document.querySelector(".main-info__contacts");
-
-let showOrHide = function (evt) {
-  if (footerNav.classList.contains("main-info__nav--closed") && footerContacts.classList.contains("main-info__contacts--closed")) {
-    if (evt.target === footerNav) {
-      footerNav.classList.remove("main-info__nav--closed")
+var showOrHide = function (evt) {
+  if (evt.target === footerNav || evt.target === footerNavHeader) {
+    if (!footerContacts.classList.contains('main-info__contacts--closed')) {
+      footerNav.classList.toggle('main-info__nav--closed');
+      footerContacts.classList.add('main-info__contacts--closed');
     } else {
-      footerContacts.classList.remove("main-info__contacts--closed")
+      footerNav.classList.toggle('main-info__nav--closed');
     }
-  } else {
-    footerNav.classList.toggle("main-info__nav--closed");
-    footerContacts.classList.toggle("main-info__contacts--closed");
   }
-}
+  if (evt.target === footerContacts || evt.target === footerContactsHeader) {
+    if (!footerNav.classList.contains('main-info__nav--closed')) {
+      footerNav.classList.add('main-info__nav--closed');
+      footerContacts.classList.toggle('main-info__contacts--closed');
+    } else {
+      footerContacts.classList.toggle('main-info__contacts--closed');
+    }
+  }
+};
 
-footerNav.addEventListener(("click"), function (e) {
+footerNav.addEventListener(('click'), function (e) {
   showOrHide(e);
 });
-footerContacts.addEventListener(("click"), function (e) {
+footerContacts.addEventListener(('click'), function (e) {
   showOrHide(e);
-})
+});
+
+var validateFieldPhone = function (evt) {
+  evt.target.value = evt.target.value.replace(phoneRegExp, '');
+  evt.target.maxLength = 16;
+  if (evt.target.value.length === 0) {
+    evt.target.value = '+';
+  }
+  if (maskPhone[evt.target.value.length] && evt.data !== null) {
+    evt.target.value = evt.target.value + maskPhone[evt.target.value.length];
+  }
+};
+
+phoneInputModal.addEventListener(('input'), validateFieldPhone);
+phoneInputForm.addEventListener(('input'), validateFieldPhone);
+
+phoneInputModal.addEventListener('focus', function (evt) {
+  evt.target.value = '+7(';
+});
+
+phoneInputForm.addEventListener('focus', function (evt) {
+  evt.target.value = '+7(';
+});
